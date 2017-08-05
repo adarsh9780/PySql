2db8597d55e28fe93271d43c4a58c0ec5b102aba
adarsh9780 <adarshmaurya7@gmail.com>
2017-08-06 02:01:50 +0530

    Minor Changes: Doc Strings revised.

--- PySql.py
+++ PySql.py
@@ -1,16 +1,18 @@
-# list the name of every table
-def print_table(cursor):
-    print()
-    print("----------> Tables in this database are :")
-    query = 'SELECT name FROM sqlite_master WHERE type="table"'
-    # self.cursor will take our commands(queries) as string and will execute it.
-    result = cursor.execute(query)
-    for var in result:
-        print(var)
-
 class InitializeTable(object):
     
     def __init__(self, table_name, cursor):
+        """
+        DESCRIPTION: Initialize the table. Contains various methods for different
+                     manipulations.
+
+        VARIABLES :  table_name--->name of table  to be passed while initializing 
+                     this class.
+
+                     cursor----> object of connection which helps to move and access
+                     the database.
+
+        USAGE :      a = InitializeTable(table_name, cursor_object)  
+        """
         self.table_name = table_name
         self.cursor = cursor
 
@@ -50,6 +52,21 @@ class InitializeTable(object):
     # pass a list of columns/features, if nothing is passed will
     # print each and every entries present in the table
     def select(self, columns='*'):
+        """
+        DESCRIPTION:  It will select entries along the given columns from the database, 
+                      if no argument is provided it will select each & every column.
+                      Similar to "SELECT col1, col2.... FROM {}"
+                                    OR
+                      Similar to "SELECT * FROM {}"
+
+        ARGUMENTS :   columns-----> Whenever & wherever 'columns' is
+                      used, it means a list of column names is to be passed.
+
+        RETURN TYPE : Nothing.
+
+        USAGE :       select_distinct(customerId) where 'customerId' is 
+                      supposed to be a coulmn name.  
+        """
         print()
         query = 'SELECT {} FROM {}'.format(self.unpack(columns), self.table_name)
         result = self.cursor.execute(query)
@@ -57,6 +74,18 @@ class InitializeTable(object):
             print(var)
 
     def select_distinct(self, feature):
+        """
+        DESCRIPTION:  It will select distinct entries in the database.
+                      Similar to "SELECT DISTINCT {} FROM {}"
+
+        ARGUMENTS :   feature-----> Whenever & wherever 'feature' is
+                      used it means a single column name is to be passed.
+
+        RETURN TYPE : Nothing
+
+        USAGE :       select_distinct(customerId) where 'customerId' is 
+                      supposed to be a coulmn name.  
+        """
         print()
         print("----------> Distinct features are: ")
         query = 'SELECT DISTINCT {} FROM {}'.format(feature, self.table_name)
@@ -65,6 +94,18 @@ class InitializeTable(object):
             print(var)
 
     def count_distinct(self, feature):
+        """
+        DESCRIPTION:  It will count distinct entries in the database.
+                      Similar to "SELECT COUNT(DISTINCT {}) FROM {}"
+
+        ARGUMENTS :   feature-----> Whenever & wherever 'feature' is
+                      used it means a single column name is to be passed.
+
+        RETURN TYPE : Nothing
+
+        USAGE :       count_distinct(customerId) where 'customerId' is 
+                      supposed to be a coulmn name.  
+        """
         print()
         print("----------> Number of distinct {} are: ".format(feature))
         query = 'SELECT COUNT(DISTINCT {}) FROM {}'.format(feature, self.table_name)
@@ -89,12 +130,15 @@ the condition {}".format(self.unpack(columns), condition))
                      to comment it out after used once, else duplicate values will be inserted
                      every time you would run your file.
 
-        USAGE      : insert(column, values)
-                     column----> list of features
-                                 Make sure to add those features which can't take NULL Values.
+        ARGUMENTS  : column----> list of features
+                     Make sure to add those features which can't take NULL Values.
 
                      values----> list of values
-                                 Make sure values are in same order as in features are passed.
+                     Make sure values are in same order as in features are passed.
+
+        RETURN TYPE: Nothing.
+        
+        USAGE      : insert(column, values)
 
         NOTE(1)    : Format of value:
                      values = ["'value1'", "'value2'", ...... so on]
@@ -112,6 +156,20 @@ the condition {}".format(self.unpack(columns), condition))
                                                        self.unpack(columns), 
                                                        self.unpack(values))
         result = self.cursor.execute(query)
+
+
+
+
+# list the name of every table
+def print_table(cursor):
+    print()
+    print("----------> Tables in this database are :")
+    query = 'SELECT name FROM sqlite_master WHERE type="table"'
+    # self.cursor will take our commands(queries) as string and will execute it.
+    result = cursor.execute(query)
+    for var in result:
+        print(var)
+
 
 if __name__ == '__main__':
     pass
